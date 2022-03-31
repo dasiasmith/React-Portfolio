dbf96010a779717b6961c824fac3cc835a012964
Dasia Smith <92009327+dasiasmith@users.noreply.github.com>
2022-03-31 18:51:38 -0500

    Updated data to add additional projects

--- react-portfolio/src/data.js
+++ react-portfolio/src/data.js
@@ -1,4 +1,6 @@
 export const Projectlist = [
     { title: "'Flix and Spill", description: "A website that allow users to leave, comment, and like reviews for Netflix shows and movies", repo: "https://github.com/jing-liu-778/Flix-and-Spill", deployed: "https://shielded-gorge-65326.herokuapp.com/" },
-    { title: "sdsvfxc", description: "sdfssvx", repo: "sdcvxfsdf", deployed: "sfsxcvbdfs" }
+    { title: "Marvel vs. DC", description: "A website that comapres Marvel superheroes vs. DC superheroes utilizing a superhero API and IMDb movie rating API", repo: "https://github.com/dasiasmith/Marvel-Vs-DC", deployed: "https://dasiasmith.github.io/Marvel-Vs-DC/" },
+    { title: "Weather Dashboard", description: "A website that allows users to input a city name and receive daily and 5-day forecast for that city", repo: "https://github.com/dasiasmith/Weather-Dashboard", deployed: "https://dasiasmith.github.io/Weather-Dashboard/" },
+    { title: "Password Generator", description: "A website that generates a passwored based on user preferences such as capitalization, special character, and numbers", repo: "https://github.com/dasiasmith/Password-Generator", deployed: "https://dasiasmith.github.io/Password-Generator/" }
 ]
